3791bb6bf0c765d86f29f01ffe727bed85cf0eea
crasars <38199147+crasars@users.noreply.github.com>
2025-05-31 12:20:40 +0200

    Create MainTest.java

---
+++ src/test/java/com/theactualgame/MainTest.java
@@ -0,0 +1,11 @@
+package com.theactualgame;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void sampleTest() {
+        assertTrue(true, "This test should always pass - Right Jaqub?");
+    }
+} 
